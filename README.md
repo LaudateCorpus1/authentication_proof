c93925151bb9b220d6ef1111a1a7901092df3872
Pirjot <apirjot@gmail.com>
2021-08-17 04:25:12 -0700

    Fun with MongoDB

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Steps to take for simple web app authentication (with PassportJS):
 
 1. Set up template files
+2. Install Express, Nodemon, MongoDB
+3. 
+
+
+. Install Sessions (npm install sessions)
--- config/index.js
+++ config/index.js
@@ -1,29 +1,38 @@
+//Load all variables from .env into process.env
+require('dotenv').config({ path: 'config/.env' });
+
 // Include Express Dependency
 const express = require('express');
+const { write } = require('fs');
+//Include Mongoose Dependency
+const mongoose = require('mongoose');
 //Initialize App
 const app = express();
 
-function middleware(req, res, next) {
-    console.log("Middleware function.");
-    next();
+//Connect to Mongoose Database
+const connection = mongoose.createConnection(process.env.DATABASE_URL,  {useNewUrlParser: true, useUnifiedTopology: true });
+
+
+//Connection variable
+connection.on('error', (error) => console.log(error));
+connection.once('open', () => console.log("Database Connected."));
+
+// Write to database
+async function writeToDatabase() {
+    const doc = { name: "Neapolitan pizza", shape: "round" };
+    const result = await connection.collection("newCollection").insertOne(doc);
+    console.log(
+        `A document was inserted with the _id: ${result.insertedId}`,
+    );
 }
 
-function standardExpressGet(req, res, next) {
-    console.log("Standard Express");
-    res.send("<h1>Hello World</h1>");
-    next();
-}
-
-app.use(middleware);
-
-//Get Method
-app.get('/', standardExpressGet);
+writeToDatabase();
 
 //Serve Static HTML pages
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 //Parse incoming JSON requests
-// app.use(express.json());
+app.use(express.json());
 
 //Set Port
 app.listen(3000, () => console.log("Listening..."));
